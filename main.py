eb467aecba91b0144acb197ad1e50fa9734e3182
redlegoman <andy@mcdade.co.uk>
2025-02-18 17:59:47 +0000

    added day to daily playlist

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import yaml
 from modules.listenbrainz_functions import get_weeklyjams_playlist,get_weeklyexploration_playlist,get_dailyjams_playlist
 from modules.plex_functions import set_section
+from datetime import date
+import calendar
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
 if __name__ == "__main__":
+    my_date = date.today()
+    today = calendar.day_name[my_date.weekday()]
+    print(today)
+
     set_section()
     get_dailyjams_playlist(cfg['user_token'])
-    get_weeklyjams_playlist(cfg['user_token'])
+    if today == "Monday":
+      get_weeklyjams_playlist(cfg['user_token'])
     #get_weeklyexploration_playlist(cfg['user_token'])
